85f8b8b58e44f81371931bd881363eeda7dad2af
Rubén Fernández <rubenfernandez@MacBook-Pro-de-Ruben.local>
2025-07-20 13:30:20 +0200

    First commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Tierra y mesa</h1>
+  </div>
+`;
