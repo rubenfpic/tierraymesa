4f18e2f3415edd43ace499abb910d5c3bdd7602f
Rubén Fernández <rubenfernandez@MacBook-Pro-de-Ruben.local>
2025-08-06 20:26:36 +0200

    Filters panel

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,7 +1,59 @@
 export function createFilters() {
   const fragment = document.createRange().createContextualFragment(`
-    
+    <div class="filters__toggle">
+      <button type="submit" class="button button--secondary button--md button--icon js-filters-toggle" aria-controls="filtersPanel" aria-expanded="false">
+        <svg aria-hidden="true" class="icon icon--24">
+          <use xlink:href="/sprite.svg#filter"></use>
+          </svg>
+        <span>Filtros</span>
+      </button>
+    </div>
+    <div class="filters__panel js-filters-panel" id="filtersPanel">
+      <div class="filters__panel-inner">
+        <div class="filters__header">
+          <h3 class="filters__title">Filtrar la búsqueda</h3>
+          <div class="filters__close">
+            <button type="button" class="button button--icon button--link js-filters-close" aria-label="Cerrar panel de filtros">
+              <svg class="icon icon--24" aria-hidden="true">
+                <use xlink:href="/sprite.svg#close"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   `);
 
   return fragment;
+}
+
+export function toggleFiltersPanel() {
+  const filtersToggle = document.querySelector(".js-filters-toggle");
+  const filtersPanel = document.querySelector(".js-filters-panel");
+  const filtersClose = document.querySelector(".js-filters-close");
+  const body = document.querySelector("body");
+
+  filtersToggle.addEventListener("click", () => {
+    const isExpanded = filtersPanel.classList.contains("is-visible");
+
+    filtersToggle.setAttribute("aria-expanded", !isExpanded);
+    filtersPanel.classList.toggle("is-visible");
+    if (
+      window.getComputedStyle(filtersPanel).position == "fixed" &&
+      filtersPanel.classList.contains("is-visible")
+    ) {
+      body.classList.add("no-scroll");
+    }
+  });
+
+  filtersClose.addEventListener("click", () => {
+    filtersPanel.classList.toggle("is-visible");
+    body.classList.remove("no-scroll");
+  });
+
+  window.addEventListener("resize", () => {
+    filtersPanel.classList.remove("is-visible");
+    filtersToggle.setAttribute("aria-expanded", false);
+    body.classList.remove("no-scroll");
+  });
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "@styles/index.scss";
 import { createHeader } from "@components/header";
 import { createFooter } from "@components/footer";
 import { createIntro } from "@components/intro";
+import { createFilters, toggleFiltersPanel } from "@components/filters";
 import { createCards, initCardDetails } from "@components/cards";
 
 const headerNode = document.getElementById("header");
@@ -11,6 +12,11 @@ headerNode.appendChild(headerContent);
 const introNode = document.getElementById("intro");
 const introContent = createIntro();
 introNode.appendChild(introContent);
+
+const filtersNode = document.getElementById("filters");
+const filtersContent = createFilters();
+filtersNode.appendChild(filtersContent);
+toggleFiltersPanel();
 
 const cardsNode = document.getElementById("cards");
 const cardsContent = createCards();
